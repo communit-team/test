95591bb0b7a6b152e99644bf4860c664070eeb36
Leo <leo@crossrider.com>
2015-01-12 19:51:17 +0200

    First commit - Rails 4 new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Communit2::Application.config.secret_key_base = '8994e0d86fe58ca0ad7c115f22a21d00d181115136264bd1b41060abe50f43ca40750f68064bd05b314142a84e53d76c6993333f8a0d36b11dcceeb661f1846f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Communit2::Application.config.session_store :cookie_store, key: '_communit2_session'
